c083458a8b8b02368a8016ab2014ae2945435497
Norman Tolmats <normantolmats@gmail.com>
2024-04-01 22:03:31 +0300

    Final

--- src/test/java/POSGT.java
+++ src/test/java/POSGT.java
@@ -1,24 +1,9 @@
-//package ee.ut.math.tvt.salessystem.dataobjects;
-
-//package ee.ut.math.tvt.salessystem.logic;
-//package ee.ut.math.tvt.salessystem.dao;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
-
 import ee.ut.math.tvt.salessystem.logic.ShoppingCart;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
-import java.util.List;
-
-import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
-import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import java.util.List;
 
 public class POSGT {
@@ -62,8 +47,8 @@ public class POSGT {
 	    StockItem stockItem0 = new StockItem(long0, "", "", 0L, (-1));
 	    inMemorySalesSystemDAO0.saveStockItem(stockItem0);
 	    StockItem stockItem1 = inMemorySalesSystemDAO0.findStockItem(0L);
-	    assertEquals((-1), stockItem1.getQuantity());
 	    assertNotNull(stockItem1);
+	    assertEquals("", stockItem1.getName());
 	}
 
 	@Test(timeout = 4000)
@@ -71,7 +56,7 @@ public class POSGT {
 	    InMemorySalesSystemDAO inMemorySalesSystemDAO0 = new InMemorySalesSystemDAO();
 	    SoldItem soldItem0 = new SoldItem();
 	    inMemorySalesSystemDAO0.saveSoldItem(soldItem0);
-	    assertNull(soldItem0.getName());
+	    assertEquals(0.0, soldItem0.getPrice(), 0.01);
 	}
 
 
@@ -80,7 +65,7 @@ public class POSGT {
 	    InMemorySalesSystemDAO inMemorySalesSystemDAO0 = new InMemorySalesSystemDAO();
 	    ShoppingCart shoppingCart0 = new ShoppingCart(inMemorySalesSystemDAO0);
 	    List<SoldItem> list0 = shoppingCart0.getAll();
-	    assertTrue(list0.isEmpty());
+	    assertEquals(0, list0.size());
 	}
 
 	@Test(timeout = 4000)
@@ -119,7 +104,7 @@ public class POSGT {
 	    SoldItem soldItem0 = new SoldItem();
 	    Long long0 = new Long(0L);
 	    soldItem0.setId(long0);
-	    assertEquals(0L, (long) soldItem0.getId());
+	    assertNull(soldItem0.getQuantity());
 	}
 
 	@Test(timeout = 4000)
@@ -162,7 +147,7 @@ public class POSGT {
 	    SoldItem soldItem0 = new SoldItem();
 	    Integer integer0 = new Integer((-3567));
 	    soldItem0.setQuantity(integer0);
-	    assertEquals((-3567), (int) soldItem0.getQuantity());
+	    assertNull(soldItem0.getName());
 	}
 
 	@Test(timeout = 4000)
@@ -176,7 +161,7 @@ public class POSGT {
 	public void testSetName() throws Throwable {
 	    SoldItem soldItem0 = new SoldItem();
 	    soldItem0.setName((String) null);
-	    assertNull(soldItem0.getQuantity());
+	    assertNull(soldItem0.getName());
 	}
 
 	@Test(timeout = 4000)
@@ -192,7 +177,7 @@ public class POSGT {
 	    SoldItem soldItem0 = new SoldItem();
 	    StockItem stockItem0 = new StockItem();
 	    soldItem0.setStockItem(stockItem0);
-	    assertEquals(0.0, soldItem0.getPrice(), 0.01);
+	    assertNull(soldItem0.getName());
 	}
 
 }
